bc3497c62f589bc7426b7f4382166321824e914f
MalondaClement <46990837+MalondaClement@users.noreply.github.com>
2022-02-22 12:32:27 +0100

    add utils files

---
+++ utils/metrics.py
@@ -0,0 +1,17 @@
+#
+# utils/metrics.py
+#
+# Clément Malonda
+
+import numpy as np
+#
+
+def iou(outputs, targets):
+    intersection = (outputs & targets).float().sum((1, 2))
+    union = (outputs | targets).float().sum(1, 2)
+
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+
+    thresholded = np.ceil(np.clip(20 * (iou - 0.5), 0, 10)) / 10
+
+    return thresholded
